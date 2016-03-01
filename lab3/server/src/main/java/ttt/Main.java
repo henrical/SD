d72096c5fd752ac4f245e25028a8a74d2ed5c64b
henrical <henrique.cal94@gmail.com>
2016-03-01 21:19:53 +0000

    completed main method

--- lab3/server/src/main/java/ttt/Main.java
+++ lab3/server/src/main/java/ttt/Main.java
@@ -6,13 +6,17 @@ import java.rmi.registry.Registry;
 
 public class Main {
 
+    private static int DEFAULT_PORT = 2048;
+
     public static void main(String args[]){
         TTT ttt;
         
         try{
             ttt = new TTT();
             
-             Registry reg = LocateRegistry.createRegistry(119);
+             Registry reg = LocateRegistry.createRegistry(DEFAULT_PORT);
+             
+             reg.rebind("ttt",ttt);
         }
         catch(RemoteException excep) {
             System.out.println(excep.toString());
