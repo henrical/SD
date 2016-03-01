ef4b1d2ba5dbfd51d33e030d3a91011f3b87a066
henrical <henrique.cal94@gmail.com>
2016-03-01 22:13:59 +0000

    configuring log4j on server

--- lab3/server/src/main/java/ttt/Main.java
+++ lab3/server/src/main/java/ttt/Main.java
@@ -4,10 +4,14 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
     private static int DEFAULT_PORT = 2048;
 
+    private static Logger log = Logger.getLogger(Main.class.getName());
+    
     public static void main(String args[]){
         TTT ttt;
         
@@ -21,6 +25,13 @@ public class Main {
         catch(RemoteException excep) {
             System.out.println(excep.toString());
             System.exit(-1);
+        }
+        
+        log.info("Server up and running.");
+        log.debug("Awaiting communication...");
+        
+        while(true){
+            
         }
         
        
