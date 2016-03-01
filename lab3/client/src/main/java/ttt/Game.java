099dd833ab6795d01acf01d56100d6d4e071910e
henrical <henrique.cal94@gmail.com>
2016-03-01 22:34:49 +0000

    client remote name lookup

--- lab3/client/src/main/java/ttt/Game.java
+++ lab3/client/src/main/java/ttt/Game.java
@@ -1,9 +1,32 @@
 package ttt;
 
+import org.apache.log4j.Logger;
+
+import java.rmi.Naming;
+
 public class Game {
 
+    private static String remote_location = "rmi://127.0.0.1:2048/ttt";
+
+    private static Logger log = Logger.getLogger(Game.class.getName());
+    
     public static void main(String[] args) throws Exception {
-    	  /* TO DO */
+        
+            TTT ttt;
+    	  
+            log.info("Starting game...");
+    	  
+    	    try {
+                ttt = (TTT) Naming.lookup(remote_location);
+    	    }
+    	    catch(Exception excep) {
+                System.out.println(excep.toString());
+    	    }
+
+            while(true){
+                //TODO: Play game
+            }
+    	  
     }
 
 }
